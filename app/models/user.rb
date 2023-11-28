30f1ebb3337fd3932343ed0f28eaaeb201bfb81a
jordi perez <jlpmccs@gmail.com>
2023-11-28 14:05:13 +0100

    seedfile modified columns missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :events
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+           :recoverable, :rememberable, :validatable
 end
