b6aeb1f0e330e27453d97b8c687fe85042be3b21
catriona beamish <mani.motamedifr@gmail.com>
2023-11-29 16:05:39 +0100

    almost finished with the new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :max_attendees)
+  end
 end
