656695d7bb4738bd678bb7c290f4e2de89b0ca0a
jordi perez <jlpmccs@gmail.com>
2023-12-04 17:25:11 +0100

    added searchbar, mirroing style missing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/profile', to: 'users#show', as: :user_profile
+  get 'gardens/search', to: 'gardens#search', as: 'search_gardens'
 
   get 'users/:id' => 'users#show'
   resources :events, only: [:index, :show] do
